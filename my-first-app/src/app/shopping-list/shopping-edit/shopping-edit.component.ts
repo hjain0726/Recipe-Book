892085b9d4257cc0aaee9db5e59d71cc6698f0ef
hjain0726 <hjain0726@gmail.com>
2018-08-29 01:02:17 +0530

    new commit

--- my-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ my-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,18 +1,20 @@
-import { Component, ViewChild, Output,EventEmitter, ElementRef} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent{
-  @Output() addIngredient=new EventEmitter<Ingredient>();
-  @ViewChild('nameInput') name:ElementRef;
-  @ViewChild('amountInput') amount:ElementRef;
-  
-   addNew(){
-   const newIngredient=new Ingredient(this.name.nativeElement.value,this.amount.nativeElement.value);
-    this.addIngredient.emit(newIngredient);
-   }
+export class ShoppingEditComponent {
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
+
+  constructor(private slservice: ShoppingListService) { }
+
+  addNew() {
+    const newIngredient = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value);
+    this.slservice.addIngredient(newIngredient);
+  }
 }
--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,17 +1,21 @@
-import { Component} from '@angular/core';
-import{ Ingredient } from '../shared/ingredient.model';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-    selector:'app-shopping-list',
-    templateUrl:'./shopping-list.component.html',
-    styleUrls:['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent{
-    ingredients:Ingredient[]=[
-        new Ingredient('apple','5'),
-        new Ingredient('tomatoes','10')
-    ];
-    onIngredientAdded(ingredient:Ingredient){
-        this.ingredients.push(ingredient);
+export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[] = [];
+
+    constructor(private slService: ShoppingListService) { }
+
+    ngOnInit() {
+        this.ingredients = this.slService.getIngredients();
+        this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+            this.ingredients = ingredients;
+        });
     }
 }
