33cd0f7d1b897e7c19e36f007781dc0db9dc0ebb
hjain0726 <hjain0726@gmail.com>
2018-08-29 00:55:25 +0530

    new commit

--- my-first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, Output,EventEmitter} from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Recipe } from '../../recipe.model'
-//import { EventEmitter } from '../../../../../node_modules/protractor';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
     selector:'app-recipe-item',
@@ -9,10 +9,11 @@ import { Recipe } from '../../recipe.model'
 })
 export class RecipeItemComponent{
     @Input() recipe:Recipe;
-    @Output() recipeSelected=new EventEmitter<void>();
-    
+
+    constructor(private recipeService:RecipeService){}
+
     onSelected(){
-        this.recipeSelected.emit();
+        this.recipeService.recipeSelected.emit(this.recipe);
     }
 
 }
