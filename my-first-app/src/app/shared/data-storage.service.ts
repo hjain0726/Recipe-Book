79787b7793a34cdafe98b35dbec3974aa4ec12d2
hjain0726 <hjain0726@gmail.com>
2018-11-18 02:03:36 +0530

    authentication added

--- my-first-app/src/app/shared/data-storage.service.ts
+++ my-first-app/src/app/shared/data-storage.service.ts
@@ -2,22 +2,29 @@ import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Http, Response } from "@angular/http";
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http,
+        private recipeService: RecipeService,
+        private authService: AuthService) { }
     storeRecipes() {
-        return this.http.put('https://recipe-book-c17b4.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://recipe-book-c17b4.firebaseio.com/recipes.json?auth=' + token,
+            this.recipeService.getRecipes());
     }
     getRecipes() {
-        this.http.get('https://recipe-book-c17b4.firebaseio.com/recipes.json')
-        .subscribe((response: Response) => {
-            const recipes: Recipe[] = response.json();
-            for(let recipe of recipes){
-                if(!recipe['ingredients']){
-                    recipe['ingredients']=[];
+        const token = this.authService.getToken();
+
+        this.http.get('https://recipe-book-c17b4.firebaseio.com/recipes.json?auth=' + token)
+            .subscribe((response: Response) => {
+                const recipes: Recipe[] = response.json();
+                for (let recipe of recipes) {
+                    if (!recipe['ingredients']) {
+                        recipe['ingredients'] = [];
+                    }
                 }
-            }
-            this.recipeService.setRecipes(recipes);
-        });
+                this.recipeService.setRecipes(recipes);
+            });
     }
 }
