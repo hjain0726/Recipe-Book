24502b2ae3d97ef5b4b9ca3a496fcb23ff743915
hjain0726 <hjain0726@gmail.com>
2019-02-10 16:18:28 +0530

    authentication frontend added

--- my-first-app/src/app/auth/auth-guard.service.ts
+++ my-first-app/src/app/auth/auth-guard.service.ts
@@ -6,9 +6,9 @@ import { AuthService } from "./auth.service";
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if(this.authService.isAuthenticated()){
+        if (this.authService.isAuthenticated()) {
             return true;
-        }else{
+        } else {
             this.router.navigate(['/signin']);
             return false;
         }
